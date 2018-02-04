6bee068d0bc4b04b715766207084ce7cda3cf900
Dmitry Chaban <dmitrychaban@yahoo.com>
2018-02-04 10:28:32 +0200

    Initial commit

---
+++ src/main/java/io/github/coffeecodehouse/opensuched/OpensuchedApplication.java
@@ -0,0 +1,12 @@
+package io.github.coffeecodehouse.opensuched;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpensuchedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpensuchedApplication.class, args);
+	}
+}
